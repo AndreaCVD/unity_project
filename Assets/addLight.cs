c3e19dac390c44b3d32d0f1adee3dafb465152f9
AndreaCVD <bluecarolina10@gmail.com>
2025-09-23 11:38:42 +0200

    modificacion del script de la luz para cambiar sus caracteristicas desde el codigo

--- Assets/addLight.cs
+++ Assets/addLight.cs
@@ -4,12 +4,30 @@ public class addLight : MonoBehaviour
 {
 
 
-    public GameObject new_light;
+    public Light luz;
+
+    // Variables públicas para controlar la luz
+    public float intensidad = 2f;
+    public Color colorLuz = Color.red;
+    public LightType tipoLuz = LightType.Point;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        luz = GetComponent<Light>();
+
+        // Verificar si el componente Light existe
+        if (luz != null)
+        {
+            // Inicializamos las características de la luz al principio
+            luz.intensity = intensidad;
+            luz.color = colorLuz;
+            luz.type = tipoLuz;
+        }
+        else
+        {
+            Debug.LogError("No se encontró el componente Light en este objeto.");
+        }
     }
 
     // Update is called once per frame
